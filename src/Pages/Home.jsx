bcc95bbd80c1151b9c14e64b4a25bf6a52d64eee
faldibule <faldibule@gmail.com>
2022-05-22 10:01:59 +0700

    Add: Tailwind css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <p>Ini Halaman Home</p>
-            <Link to="/">
-                Back
-            </Link>
+            <div className="min-h-screen flex justify-center items-center flex-col">
+                <h1 className="text-3xl font-bold text-blue-600">
+                    Home Page
+                </h1>
+                <Link className='bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800' to="/">
+                    Back
+                </Link>
+            </div>
 
         </div>
     );
--- src/Pages/Welocome.jsx
+++ src/Pages/Welocome.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom';
 const Welocome = () => {
     return (
         <div>
-            <p>Selamat Datang Di Aplikasi React dengan Vite</p>
-            <Link to="/home">
-                Home
-            </Link>
+            <div className="min-h-screen flex justify-center items-center flex-col">
+                <h1 className="text-3xl font-bold text-blue-600">
+                    Welcome to React App created with Vite & Tailwind
+                </h1>
+                <Link className='bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800' to="/home">
+                    Home
+                </Link>
+            </div>
         </div>
     );
 };
