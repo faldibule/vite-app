f829d8d59dfb0b5fc4cadb932d598822491f2c62
faldibule <faldibule@gmail.com>
2022-05-30 01:00:31 +0700

    Add: test

--- src/Component/Jumbo.jsx
+++ src/Component/Jumbo.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import Profil from '../Images/profil.jpg'
 
 const navigation = [
-  { name: 'Product', href: '#' },
+  { name: 'Profil', href: '#jumbo' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
@@ -13,7 +14,7 @@ const navigation = [
 
 const Jumbo = () => {
     return (
-        <div className="relative bg-white overflow-hidden">
+        <div id="jumbo" className="relative bg-white overflow-hidden">
             <div className="max-w-7xl mx-auto">
                 <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
                 <svg
@@ -53,20 +54,17 @@ const Jumbo = () => {
                             {item.name}
                             </a>
                         ))}
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                            Log in
-                        </a>
                         </div>
                     </nav>
                     </div>
 
                     <Transition
-                    as={Fragment}
-                    enter="duration-150 ease-out"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="duration-100 ease-in"
-                    leaveFrom="opacity-100 scale-100"
+                        as={Fragment}
+                        enter="duration-150 ease-out"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="duration-100 ease-in"
+                        leaveFrom="opacity-100 scale-100"
                     leaveTo="opacity-0 scale-95"
                     >
                     <Popover.Panel
@@ -113,40 +111,22 @@ const Jumbo = () => {
 
                 <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                     <div className="sm:text-center lg:text-left">
-                    <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                        <span className="block xl:inline">Welcome</span>{' '}
-                        <span className="block text-indigo-600 xl:inline">online business</span>
-                    </h1>
-                    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                        fugiat veniam occaecat fugiat aliqua.
-                    </p>
-                    <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                        <div className="rounded-md shadow">
-                        <a
-                            href="#"
-                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                        >
-                            Get started
-                        </a>
-                        </div>
-                        <div className="mt-3 sm:mt-0 sm:ml-3">
-                        <a
-                            href="#"
-                            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                        >
-                            Live demo
-                        </a>
-                        </div>
-                    </div>
+                        <span className="block tracking-tight font-extrabold text-gray-900 xs:text-xl md:text-xl">Mahasiswa, Penggemar Koding</span>
+                        <span className="block text-indigo-600 xl:inline text-5xl tracking-tight font-extrabold xs:text-10xl md:text-6xl">
+                            Faldi Nur Ikhsan
+                        </span>
+                        
+                        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                            Saya adalah seorang mahasiswa akhir jurusan informatika. Saya berkuliah di Universitas Indraprasta PGRI. Sejak semester 5, saya mulai mendalami bidang "Pemprograman Website" karena menurut saya bidang ini adalah bidang yang cukup menjanjikan di masa depan.
+                        </p>
                     </div>
                 </main>
                 </div>
             </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                 <img
-                className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+                className="h-56 w-full object-cover sm:h-72 md:h-80 lg:w-full lg:h-full"
+                src={Profil}
                 alt=""
                 />
             </div>
